1df50c63fe272a5d433eb07a0cd013e1c34ad4e6
James Perkins <jamesperkins@hey.com>
2022-06-18 17:16:19 -0400

    adding context for no reason

--- functions/addcomment.js
+++ functions/addcomment.js
@@ -6,7 +6,7 @@ const CORS_HEADERS = {
     'Origin, X-Requested-With, Content-Type, Accept'
 };
 
-export async function handler(event) {
+export async function handler(event, context) {
   console.log(event);
   if (event.httpMethod === 'OPTIONS') {
     return {
