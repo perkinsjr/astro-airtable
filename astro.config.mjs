2801449d516259dd2b4164858393d6df557c907c
James Perkins <jamesperkins@hey.com>
2022-06-20 08:55:54 -0400

    adding build?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), react(), tailwind()],
+  build: {
+    site: 'https://dancing-kangaroo-d733b7.netlify.app/'
+  },
   site: process.env.NETLIFY ? process.env.URL : 'http://localhost:8888'
 });
